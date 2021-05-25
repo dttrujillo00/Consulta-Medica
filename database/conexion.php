3bb3c61d24818bffe5fe54545cbc4d6557337f4e
Henri <000312hdaniel@gmail.com>
2021-05-24 23:35:18 -0400

    cambiando metodo mysqli a pdo

--- database/conexion.php
+++ database/conexion.php
@@ -1,10 +1,21 @@
 <?php
-
+/* 
 define('DB_USUARIO', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'consultamedica');
 
 $conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NAME);
+ */
 
+$link = 'mysql:host=localhost;dbname=consultamedica';
+$user = 'root';
+$pass = '';
+
+try{
+$pdo=new PDO($link,$user,$pass);
+
+}catch(PDOException $e){
+print "Error";
+}
 // echo $conn->ping();
