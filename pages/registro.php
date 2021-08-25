20836afda782d05652e145d2fe4552b0ffabbbe0
Daniel <dttrujillo00@gmail.com>
2021-08-25 10:33:32 -0400

    To make push

--- pages/registro.php
+++ pages/registro.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-if ($_SESSION['rol'] != 'Administrador') {
-	header('location:../index.php');
-}
+// if ($_SESSION['rol'] != 'Administrador') {
+// 	header('location:../index.php');
+// }
 
 ?>
 
